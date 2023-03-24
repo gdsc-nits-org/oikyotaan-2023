626caf992e2d5357d4f61202f142dd2f275a3d0a
Mrinalkbanik01 <manukbanik30@gmail.com>
2023-03-24 18:30:46 +0530

    Made Certain changes

--- src/Components/Landing/Landing.jsx
+++ src/Components/Landing/Landing.jsx
@@ -3,6 +3,13 @@ import style from "./landing.module.scss";
 const Landing = () => {
   return (
     <div className={style.landing}>
+      <div className={style.birdL}>
+        <img src="/assets/birdleft.svg" alt="left_bird" />
+      </div>
+
+      <div className={style.birdR}>
+        <img src="/assets/birdright.svg" alt="right_bird" />
+      </div>
       <div className={style.Eye}>
         <img src="/assets/eye.svg" alt="eye" />
       </div>
@@ -13,14 +20,6 @@ const Landing = () => {
 
       <div className={style.boatR}>
         <img src="/assets/boatright.svg" alt="right_boat" />
-      </div>
-
-      <div className={style.birdL}>
-        <img src="/assets/birdleft.svg" alt="left_bird" />
-      </div>
-
-      <div className={style.birdR}>
-        <img src="/assets/birdright.svg" alt="right_bird" />
       </div>
     </div>
   );
