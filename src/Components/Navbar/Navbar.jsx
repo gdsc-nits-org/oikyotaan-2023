5c408b2d6a49b20dc9f83500f8e46a2a90705828
Priyajit27 <priyajitpaul27@gmail.com>
2023-04-03 01:57:05 -0800

    hamburger menu added for mobile-view

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,28 +1,55 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import { FaBars, FaArrowLeft } from "react-icons/fa";
 import style from "./Navbar.module.scss";
 import { useMediaQuery } from "../../Hooks";
-
 const Navbar = () => {
+  const [mobile, setMobile] = useState(false);
   const isMobile = useMediaQuery("(max-width: 700px)");
   return (
     <div className={style.container}>
-      <ul className={style.nav}>
-        <li>
+      <div className={style.new}>
+        <img
+          src="/images/oikyotan.svg"
+          alt="img"
+          className={mobile === false ? style.img : style.imgmob}
+        />
+      </div>
+      {/* <ul className={style.nav}> */}
+      <ul className={mobile === false ? style.nav : style.navmob}>
+        <li className={style.li1}>
+          <img src="/images/home.png" alt="img" className={style.imgm} />
           <Link to="/">Home</Link>
         </li>
         <li>
+          <img src="/images/clipboard.png" alt="img" className={style.imgm} />
           <Link to="/events">Events</Link>
         </li>
         <li>
-          <img src="/images/oikyotan.svg" alt="img" width={isMobile ? 100 : 140} />
+          <img
+            src="/images/oikyotan.svg"
+            alt="img"
+            width={isMobile ? 0 : 140}
+            className={style.img1}
+          />
         </li>
-        <li>
+        <li className={style.li5}>
+          <img src="/images/image.png" alt="img" className={style.imgm} />
           <Link to="/gallery">Gallery</Link>
         </li>
         <li>
+          <img src="/images/users.png" alt="img" className={style.imgm} />
           <Link to="/team">Team</Link>
         </li>
       </ul>
+
+      <button className={style.mobileview} onClick={() => setMobile(!mobile)}>
+        {mobile ? (
+          <FaArrowLeft className={style.cross1} />
+        ) : (
+          <FaBars className={style.cross} />
+        )}
+      </button>
     </div>
   );
 };
