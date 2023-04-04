bce86e5fece86d7d51947d4ccbd049266b4ba62c
Rohan Jha <rohan21_ug@ece.nits.ac.in>
2023-04-05 02:49:30 +0530

    add:audio added for landing page

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,22 @@
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
+/* eslint-disable jsx-a11y/media-has-caption */
+import { useRef, useState } from "react";
 import style from "./Home.module.scss";
 import { Landing, Sponsor } from "../../Components";
 
 const Home = () => {
+  const audioRef = useRef(null);
+  const [play, setPlay] = useState(false);
+  const toggle = () => {
+    if (!play) {
+      audioRef.current.play();
+      setPlay(true);
+    }
+  };
   return (
-    <main className={style.home}>
+    <main className={style.home} onClick={toggle}>
+      <audio src="/assets/intro.mp3" autoPlay ref={audioRef}></audio>
       <Landing />
       <Sponsor />
     </main>
