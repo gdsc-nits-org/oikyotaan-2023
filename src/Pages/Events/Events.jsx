85bedb0ee0432d9577f3018b53550f321e4a6085
Jugya K. Gogoi <crjugya123@gmail.com>
2023-04-04 11:20:18 +0530

    fix: flower alignment

--- src/Pages/Events/Events.jsx
+++ src/Pages/Events/Events.jsx
@@ -8,8 +8,14 @@ const Events = () => {
       <img className={style.bimgLeft} alt="flower" src="/Gallery/flowers.png" />
       <img className={style.bimgRight} alt="flower" src="/Gallery/flowers.png" />
       <div className={style.text}>
+        <div className={style.design}>
+          <img src="/Gallery/design.png" alt="design" />
+        </div>
         <p>Events</p>
-        <img src="/images/underline.svg" alt="underline" />
+        {/* <img src="/images/underline.svg" alt="underline" /> */}
+        <div className={style.design}>
+          <img src="/Gallery/design.png" alt="design" />
+        </div>
       </div>
       <div className={style.cardcontainer}>
         <VerticalCarousel />
